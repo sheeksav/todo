dd3bfcf85964605c3fd74925e259f1183b37d5d5
sheeksav <andy.sheeks@gmail.com>
2014-09-19 16:48:44 -0400

    Added string methods to the models.

--- engine/models.py
+++ engine/models.py
@@ -4,11 +4,20 @@ from django.contrib.auth.models import User
 class ToDoList(models.Model):
     owner = models.ForeignKey(User)
 
+    def __str__(self):
+        return u'To-Do List for %s' % self.owner.get_full_name()
+
+
 class ToDoItem(models.Model):
     list = models.ForeignKey(ToDoList)
     description = models.CharField(max_length=300)
     complete = models.BooleanField(default=False)
     updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+
+
 
 
 
